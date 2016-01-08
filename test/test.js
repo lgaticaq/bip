3e8aee62dc71a81664fff03f34c40d4a224a838a
Leonardo Gatica <lgatica@protonmail.com>
2016-01-08 22:02:27 +0000

    Simplify test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,5 @@
 'use strict';
 
-import path from 'path';
-
 import {expect} from 'chai';
 import nock from 'nock';
 
@@ -18,14 +16,25 @@ describe('bip', () => {
       nock.disableNetConnect();
       nock('http://www.metrosantiago.cl')
         .get(`/contents/guia-viajero/includes/consultarTarjeta/${number}`)
-        .replyWithFile(200, path.join(__dirname, 'valid.json'));
+        .reply(200, [
+          {
+            'estado': 0,
+            'mensaje': 'Tarjeta Valida'
+          },
+          {
+            'salida': true,
+            'tarjeta': '11111111',
+            'saldo': '1180',
+            'fecha': '02\/01\/2016 20:59'
+          }
+        ]);
     });
 
     it('should return a balance data (callback)', (done) => {
       lib(number, (err, data) => {
         expect(err).to.be.null;
         expect(data).to.be.a('object');
-        expect(data.number).to.be.a('number');
+        expect(data.number).to.eql(number);
         expect(data.balance).to.be.a('number');
         expect(data.date).to.be.a('date');
         expect(data.message).to.be.a('string');
@@ -38,7 +47,7 @@ describe('bip', () => {
     it('should return a balance data (promise)', (done) => {
       lib(number).then((data) => {
         expect(data).to.be.a('object');
-        expect(data.number).to.be.a('number');
+        expect(data.number).to.eql(number);
         expect(data.balance).to.be.a('number');
         expect(data.date).to.be.a('date');
         expect(data.message).to.be.a('string');
@@ -59,14 +68,23 @@ describe('bip', () => {
       nock.disableNetConnect();
       nock('http://www.metrosantiago.cl')
         .get(`/contents/guia-viajero/includes/consultarTarjeta/${number}`)
-        .replyWithFile(200, path.join(__dirname, 'invalid.json'));
+        .reply(200, [
+          {
+            'estado': 1,
+            'mensaje': 'Esta tarjeta no se puede cargar'
+          },
+          {
+            'salida': false,
+            'tarjeta': '11111111'
+          }
+        ]);
     });
 
     it('should return a empty data (callback)', (done) => {
       lib(number, (err, data) => {
         expect(err).to.be.null;
         expect(data).to.be.a('object');
-        expect(data.number).to.be.a('number');
+        expect(data.number).to.eql(number);
         expect(data.balance).to.eql(0);
         expect(data.date).to.eql(null);
         expect(data.message).to.be.a('string');
@@ -77,7 +95,7 @@ describe('bip', () => {
 
     it('should return a empty data (promise)', (done) => {
       lib(number).then((data) => {
-        expect(data.number).to.be.a('number');
+        expect(data.number).to.eql(number);
         expect(data.balance).to.eql(0);
         expect(data.date).to.eql(null);
         expect(data.message).to.be.a('string');
