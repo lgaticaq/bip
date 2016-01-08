aba3c33360d596d3257ea1ce233fc0dcb087d224
Leonardo Gatica <lgatica@protonmail.com>
2016-01-08 22:02:10 +0000

    Add verify number

--- src/index.js
+++ src/index.js
@@ -6,22 +6,32 @@ import rp from 'request-promise';
 
 const getBalance = (number, cb) => {
   const deferred = Q.defer();
-  const options = {
-    url: `http://www.metrosantiago.cl/contents/guia-viajero/includes/consultarTarjeta/${number}`,
-    json: true
-  };
-  rp(options).then((results) => {
-    if (results.length === 0) deferred.reject(new Error('Not found'));
-    const balance = parseInt(results[1].saldo || 0, 10);
-    const date = results[1].fecha ? moment(results[1].fecha, 'DD\/MM\/YYYY HH:mm').toDate() : null;
+  if (/^\d{8}$/.test(number)) {
+    const options = {
+      url: `http://www.metrosantiago.cl/contents/guia-viajero/includes/consultarTarjeta/${number}`,
+      json: true
+    };
+    rp(options).then((results) => {
+      if (results.length === 0) deferred.reject(new Error('Not found'));
+      const balance = parseInt(results[1].saldo || 0, 10);
+      const date = results[1].fecha ? moment(results[1].fecha, 'DD\/MM\/YYYY HH:mm').toDate() : null;
+      deferred.resolve({
+        number: number,
+        balance: balance,
+        date: date,
+        message: results[0].mensaje,
+        valid: results[1].salida
+      });
+    }).catch((err) => deferred.reject(err));
+  } else {
     deferred.resolve({
-      number: parseInt(number, 10),
-      balance: balance,
-      date: date,
-      message: results[0].mensaje,
-      valid: results[1].salida
+      number: number,
+      balance: '$ 0',
+      date: null,
+      message: 'Número de tarjeta invalido. Deben ser 8 números',
+      valid: false
     });
-  }).catch((err) => deferred.reject(err));
+  }
   deferred.promise.nodeify(cb);
   return deferred.promise;
 };
