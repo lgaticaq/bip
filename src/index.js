4c2b2d88cddc42f15138f6aa68efc05f9c087749
Leonardo Gatica <lgatica@protonmail.com>
2017-06-19 22:55:03 +0000

    chore(package): update dependencies
    Migrate to standard
    Update travis
    Remove yarn

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
-'use strict';
+'use strict'
 
-const http = require('http');
+const http = require('http')
 
 module.exports = number => {
   return new Promise((resolve, reject) => {
-    const url = `http://www.metrosantiago.cl/contents/guia-viajero/includes/consultarTarjeta/${number}`;
-    http.get(url, res => {
+    const options = {
+      hostname: 'www.metrosantiago.cl',
+      port: 80,
+      path: `/contents/guia-viajero/includes/consultarTarjeta/${number}`,
+      method: 'GET'
+    }
+    const req = http.request(options, res => {
       if (res.statusCode !== 200) {
-        res.resume();
-        reject(new Error(`Request Failed. Status Code: ${res.statusCode}`));
+        reject(new Error(`Request Failed. Status Code: ${res.statusCode}`))
       } else {
-        res.setEncoding('utf8');
-        let rawData = '';
+        res.setEncoding('utf8')
+        let rawData = ''
         res.on('data', chunk => {
-          rawData += chunk;
-        });
+          rawData += chunk
+        })
         res.on('end', () => {
           try {
-            const data = JSON.parse(rawData);
-            if (data.length === 0) reject(Error('Not found'));
-            if (data[0].estado !== 0) reject(Error(data[0].mensaje));
-            const balance = parseInt(data[1].saldo || 0, 10);
-            let date = null;
+            const data = JSON.parse(rawData)
+            if (data.length === 0) reject(Error('Not found'))
+            if (data[0].estado !== 0) reject(Error(data[0].mensaje))
+            const balance = parseInt(data[1].saldo || 0, 10)
+            let date = null
             if (data[1].fecha) {
               const iso = data[1].fecha.replace(
-                /(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2})/, '$3-$2-$1T$4:$5:00-03:00');
-              date = new Date(iso);
+                /(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2})/,
+                '$3-$2-$1T$4:$5:00-03:00'
+              )
+              date = new Date(iso)
             }
             resolve({
               number: number,
@@ -33,12 +39,14 @@ module.exports = number => {
               date: date,
               message: data[0].mensaje,
               valid: data[1].salida
-            });
+            })
           } catch (err) {
-            reject(err);
+            reject(err)
           }
-        });
+        })
       }
-    }).on('error', err => reject(err));
-  });
-};
+    })
+    req.on('error', err => reject(err))
+    req.end()
+  })
+}
--- test/test.js
+++ test/test.js
@@ -1,103 +1,103 @@
-'use strict';
+'use strict'
 
-const expect = require('chai').expect;
-const nock = require('nock');
+const expect = require('chai').expect
+const nock = require('nock')
 
-const lib = require('../src');
+const lib = require('../src')
 
 describe('bip', () => {
   describe('valid', () => {
     beforeEach(() => {
-      nock.disableNetConnect();
+      nock.disableNetConnect()
       nock('http://www.metrosantiago.cl')
         .get('/contents/guia-viajero/includes/consultarTarjeta/11111111')
         .reply(200, [
-          {'estado': 0, 'mensaje': 'Tarjeta Valida'},
+          { estado: 0, mensaje: 'Tarjeta Valida' },
           {
-            'salida': true,
-            'tarjeta': '11111111',
-            'saldo': '1180',
-            'fecha': '02\/01\/2016 20:59'
+            salida: true,
+            tarjeta: '11111111',
+            saldo: '1180',
+            fecha: '02/01/2016 20:59'
           }
-        ]);
-    });
+        ])
+    })
 
     it('should return a balance data', done => {
       lib('11111111').then(data => {
-        expect(data.number).to.eql('11111111');
-        expect(data.balance).to.eql(1180);
-        expect(data.date).to.be.a('date');
-        expect(data.message).to.eql('Tarjeta Valida');
-        expect(data.valid).to.be.true;
-        done();
-      });
-    });
-  });
+        expect(data.number).to.eql('11111111')
+        expect(data.balance).to.eql(1180)
+        expect(data.date).to.be.a('date')
+        expect(data.message).to.eql('Tarjeta Valida')
+        expect(data.valid).to.eql(true)
+        done()
+      })
+    })
+  })
 
   describe('wrong status', () => {
     beforeEach(() => {
-      nock.disableNetConnect();
+      nock.disableNetConnect()
       nock('http://www.metrosantiago.cl')
         .get('/contents/guia-viajero/includes/consultarTarjeta/11111111')
         .reply(200, [
-          {'estado': 1, 'mensaje': 'Esta tarjeta no se puede cargar'},
-          {'salida': false, 'tarjeta': '11111111'}
-        ]);
-    });
+          { estado: 1, mensaje: 'Esta tarjeta no se puede cargar' },
+          { salida: false, tarjeta: '11111111' }
+        ])
+    })
 
     it('should return an error', done => {
-      lib('11111111').catch((err) => {
-        expect(err.message).to.eql('Esta tarjeta no se puede cargar');
-        done();
-      });
-    });
-  });
+      lib('11111111').catch(err => {
+        expect(err.message).to.eql('Esta tarjeta no se puede cargar')
+        done()
+      })
+    })
+  })
 
   describe('wrong status code', () => {
     beforeEach(() => {
-      nock.disableNetConnect();
+      nock.disableNetConnect()
       nock('http://www.metrosantiago.cl')
         .get('/contents/guia-viajero/includes/consultarTarjeta/11111111')
-        .reply(301);
-    });
+        .reply(301)
+    })
 
     it('should return an error', done => {
       lib('11111111').catch(err => {
-        expect(err.message).to.eql('Request Failed. Status Code: 301');
-        done();
-      });
-    });
-  });
+        expect(err.message).to.eql('Request Failed. Status Code: 301')
+        done()
+      })
+    })
+  })
 
   describe('wrong data', () => {
     beforeEach(() => {
-      nock.disableNetConnect();
+      nock.disableNetConnect()
       nock('http://www.metrosantiago.cl')
         .get('/contents/guia-viajero/includes/consultarTarjeta/11111111')
-        .reply(200, []);
-    });
+        .reply(200, [])
+    })
 
     it('should return an error', done => {
       lib('11111111').catch(err => {
-        expect(err.message).to.eql('Not found');
-        done();
-      });
-    });
-  });
+        expect(err.message).to.eql('Not found')
+        done()
+      })
+    })
+  })
 
   describe('server error', () => {
     beforeEach(() => {
-      nock.disableNetConnect();
+      nock.disableNetConnect()
       nock('http://www.metrosantiago.cl')
         .get('/contents/guia-viajero/includes/consultarTarjeta/11111111')
-        .replyWithError('Server error');
-    });
+        .replyWithError('Server error')
+    })
 
     it('should return an error', done => {
       lib('11111111').catch(err => {
-        expect(err.message).to.eql('Server error');
-        done();
-      });
-    });
-  });
-});
+        expect(err.message).to.eql('Server error')
+        done()
+      })
+    })
+  })
+})
